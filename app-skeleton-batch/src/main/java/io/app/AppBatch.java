eb60d5ad06cfecc30a917aca9e24874d72568407
alexis <dondonalexis@hotmail.fr>
2016-12-28 18:33:56 +0100

    .settings

--- app-skeleton-batch/src/main/java/io/app/AppBatch.java
+++ app-skeleton-batch/src/main/java/io/app/AppBatch.java
@@ -1,6 +1,5 @@
 package io.app;
 
-
 import io.spring.helpers.configuration.Constants;
 import io.spring.helpers.configuration.DefaultConfigurationUtil;
 
@@ -10,11 +9,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AppBatch {
 
-	
 	public static void main(String[] args) {
 		SpringApplication app = new SpringApplication(AppBatch.class);
 		app.setWebEnvironment(false);
-		DefaultConfigurationUtil.addConfiguration(app, Constants.CORE_CONF, Constants.BATCH_CONF, Constants.EXTERNAL_CONF);
+		DefaultConfigurationUtil.addConfiguration(app, Constants.CORE_CONF,
+				Constants.BATCH_CONF, Constants.EXTERNAL_CONF);
 		app.run(args);
 	}
 }
